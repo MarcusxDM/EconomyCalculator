9c044a4eb8ffb020f6323b35966f628f04ac198d
Marcus Vinicius <marcuspestana@ic.ufal.br>
2017-05-10 15:31:57 -0300

    Minor changes
    Fully Working

--- EconomyCalc.c
+++ EconomyCalc.c
@@ -3,22 +3,22 @@
 #include <stdlib.h>
 
 main(){
-    float saldo, gastosSemLanche, gastosComLanche, nseg, nter;
+    float saldo=0.0, gastosSemLanche=0.0, gastosComLanche=0.0, nseg=0.0, nter=0.0;
     int exit = 0;
     char resp;
 
     while(exit == 0){
-        printf("############## Economy Calculator 2.0 ##############\n\nSegundas e Quartas:\nPassagem: $9\nLanche: $5(opcional)\nAlmoco: $2,50\n\n");
+        printf("############## Economy Calculator 2.0 ##############\n\nSegundas e Quartas:\nPassagem: $9\nLanche: $6(opcional)\nAlmoco: $2,50\n\n");
         printf("Tercas, Quintas e Sextas:\nPassagem: $7\n\n");
         printf("Quantas segundas e quartas?\n");
         scanf("%f", &nseg);
-        printf("Quantas tercas, quintas e ?\n");
+        printf("Quantas tercas, quintas e sextas?\n");
         scanf("%f", &nter);
         printf("Qual seu saldo?\n");
         scanf("%f", &saldo);
 
-        gastosSemLanche = (nseg+(9+2.5))+(nter*(7));
-        gastosComLanche = (nseg*(9+5+7))+(nter*(7));
+        gastosSemLanche = (nseg*(2+2.5+3.5+3.5))+(nter*(7));
+        gastosComLanche = (nseg*(2+6+2.5+3.5+3.5))+(nter*(7));
         printf("\n### SUA ECONOMIA ###\n\nSem lanche: %.2f\nResto: %.2f\n", gastosSemLanche, saldo-gastosSemLanche);
         printf("Com lanche: %.2f\nResto: %.2f\n\n", gastosComLanche, saldo-gastosComLanche);
 
